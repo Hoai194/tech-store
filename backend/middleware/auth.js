62dfe90fc3aeab7b9d090316238d68da66293923
Hoai194 <tranhoai194205@gmail.com>
2026-03-17 22:38:43 +0700

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -11,6 +11,9 @@ const authUser = async (req, res, next) => {
         const token = authHeader.split(' ')[1]  
 
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        if (!decoded.id) {
+          return res.json({ success: false, message: "Invalid user token" });
+        }
         req.userId = decoded.id;
 
         next();
